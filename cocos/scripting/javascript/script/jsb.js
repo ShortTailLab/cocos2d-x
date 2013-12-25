4185fd156c13cbf176aa5e49c7c9160800d17665
colprog <colprog@gmail.com>
2013-12-25 18:33:03 +0800

    fix jsb

--- cocos/scripting/javascript/script/jsb.js
+++ cocos/scripting/javascript/script/jsb.js
@@ -6,12 +6,12 @@
 executeScript('jsb_cocos2d_constants.js');
 executeScript('jsb_cocos2d.js');
 executeScript('jsb_cocos2d_extension.js');
-//executeScript('jsb_cocos2d_studio.js');
-//executeScript('jsb_cocos2d_gui.js');
+executeScript('jsb_cocos2d_studio.js');
+executeScript('jsb_cocos2d_gui.js');
 executeScript('jsb_chipmunk_constants.js');
 executeScript('jsb_chipmunk.js');
 executeScript('jsb_opengl_constants.js');
 executeScript('jsb_opengl.js');
-//executeScript('jsb_cocosbuilder.js');
+executeScript('jsb_cocosbuilder.js');
 executeScript('jsb_sys.js');
 executeScript('jsb_deprecated.js');
